b37600eb029ff269dcf19078a576ffda78a4ebf7
Markus Skyttner <markus.skyttner@nrm.se>
2018-03-22 10:35:52 +0100

    update migration scripts

--- exec/media_migrate.R
+++ exec/media_migrate.R
@@ -1,144 +1,178 @@
 #!/usr/bin/Rscript
 
-require("biotools")
+library(RMySQL)
+library(pool)
+library(tidyverse)
+library(tibble)
+library(stringr)
+library(magrittr)  
 
-pkgs <- c(
-  "RMySQL",
-  "plyr", # for vectorised iterative functions
-  "dplyr",  # for quick data frame operations
-  "stringr"  # for regexp parsing functions
+#if (interactive()) 
+#  Sys.setenv(DINAPASS = askForPassword())
+
+pool <- dbPool(
+  drv = RMySQL::MySQL(),
+  dbname = "taxonpages_v2",
+  host = "127.0.0.1",
+  port = 3306,
+  username = "naturalist",
+  password = rstudioapi::askForPassword() #   password = Sys.getenv("DINAPASS")
 )
 
-import(pkgs)
+con <- poolCheckout(pool)
+#my_db <- connectMediaDB(db_hostname, db_user, db_port, db_pass)
+#my_db <- poolCheckout(pool)
+dbGetQuery(con,'SET NAMES utf8')
 
-# set global option to not use factors
-# this has side-effect on read.csv
-options(stringsAsFactors = FALSE)
-options(row.names = FALSE)
+media_query <- 
+"select 
+  uuid as media_uuid, 
+  media_type,
+  copyright_owner,
+  comment,
+  copyright_owner_link,
+  original_title,
+  original_title_link,
+  l.id as license_id,
+  l.abbrev as license_abbrev, 
+  l.issuer as license_short, 
+  l.uri as license_url, 
+  l.name as license_type,
+  tml.legend,
+  tml.language
+from 
+  media m 
+  left join licence l on m.licence_id = l.id
+  left join taxonpage_media tm on tm.media_uuid = m.uuid
+  left join taxonpage_media_legend tml on tml.taxonpage_media_id = tm.id
+where 
+  media_type <> 'text/html'
+order by
+  media_uuid asc
+"
 
-# this defines the command line options
-args <- "db_url, d, 2, character, source URL for db with media metadata
-src_url, s, 2, character, source URL for The Naturalist server
-dest_url, m, 2, character, dest URL for service to write media data to"
+media <- as_tibble(collect(dbGetQuery(con, media_query)))
 
-# defaults
+link_query <- 
+"select 
+  tm.taxonpage_id as page_id,
+  s.name as page_status,
+  tp.taxon_uuid,
+  tm.sort_order,
+  tm.media_uuid
+from
+  taxonpage_media tm 
+  left join taxonpage tp on tp.id = tm.taxonpage_id
+  left join status_type s on tp.status_id = s.id
+  inner join (select max(taxonpage.id) as page_id from taxonpage
+              left join taxonpage_media on taxonpage.id = taxonpage_media.taxonpage_id
+              group by taxon_uuid, language) tt on tt.page_id = tp.id
+order by
+  page_id asc, taxon_uuid asc, sort_order asc"
+    
+link <- as_tibble(collect(dbGetQuery(con, link_query)))
+poolClose(pool)
 
-opt <- get_args(args)
-
-db_url <- "dina-db.nrm.se:3306"
-src_url <- "http://test.naturforskaren.se/naturalist"
-dest_url <- "http://ingimar-hp-elitebook-8470p.nrm.se:8080/MediaServerResteasy/"
-
-if (!is.null(opt$db_url)) {
-  db_url <- opt$db_url
-} else {
-  message("Using default db URL: ", db_url)
-}
-
-if (!is.null(opt$src_url)) {
-  src_url <- opt$src_url
-} else {
-  message("Using default src URL: ", src_url)
-}
-
-if (!is.null(opt$dest_url)) {
-  dest_url <- opt$dest_url
-} else {
-  message("Using default dest URL: ", dest_url)
-}
-
-cfgfile <- system.file("exec/media.conf", package = "biotools")
-if (!interactive()) cfgfile <- "media.conf"
-#if (interactive()) cfgfile <- "media.conf"
-setwd("exec")
-if (file.exists(cfgfile)) {
-  message("Found config file, ", cfgfile, " overriding and using settings therein")
-  cfg <- get_config(cfgfile)
-  db_url <- cfg$db_url
-  db_pass <- cfg$db_pass
-  db_user <- cfg$db_user
-  src_url <- cfg$src_url
-  dest_url <- cfg$dest_url
-}
-
-# Processing media metadata from database, 
-# migrating data to new mediaserver
-
-db_port <- as.numeric(str_replace(str_extract(db_url, ":(\\d)+"), ":", ""))
-if (is.na(db_port)) db_port = 3306
-message("Using db port ", db_port)
-
-db_hostname <- str_replace(db_url, "(.*?):.*", "\\1")
-message("Using db hostname ", db_hostname)
-
-my_db <- connectMediaDB(db_hostname, db_user, db_port, db_pass)
-media <- getMediaDetailsFromDB(my_db)
-link <- getMediaLinksFromDB(my_db)
-
-uuids <- data.frame(link)
+uuids <- link
 taxa_uuids <- unique(uuids$taxon_uuid)
 
-# Retrieve all media metadata associated with the unique 
-# taxon identifier provided
-getMediaUuidsForTaxonUuidFromDB <- function(uuid) {
-  taxon_link <- data.frame(filter(link, 
-    taxon_uuid == uuid))
-  taxon_media <- subset(data.frame(media), 
-    media_uuid %in% taxon_link$media_uuid)
-  tm <- unique(taxon_media)  
-  # TODO: tm must be returned with media uuids ordered in the 
-  # right way, according to this order   media_ids <- paste(collapse = ", ", 
-  # unique(subset(uuids, taxon_uuid == t))$media_uuid)  
-  tm_sorted <- join(taxon_link, tm, by = c("media_uuid"), type = "inner")
-  return (unique(tm_sorted))
+media_from_taxon <- function(id) {
+
+  ids <- 
+    link %>% 
+    filter(taxon_uuid %in% id) %>%
+    .$media_uuid
+  
+  media_df <- 
+    media %>%
+    filter(media_uuid %in% ids) %>%
+    distinct()
+    
+  tm <- 
+    link %>% 
+    filter(taxon_uuid %in% id) %>%
+    inner_join(media_df) %>%
+    distinct()
+  
+  return(tm)
 }
 
+mt <- 
+  media_from_taxon(uuids$taxon_uuid) %>%
+  mutate(mime = sapply(str_split(media_type, fixed("/")), "[", 2)) %>%
+  mutate(url = paste0("https://dina-web.net/naturalist/media/get-stream/", media_uuid)) %>%
+  select(
+    taxon_uuid, media_uuid,
+    copyrightOwner = copyright_owner,
+    license = license_abbrev,
+    title = legend,
+    fileName = media_uuid,
+    url,
+    mime,
+    language = language,
+    sortOrder = sort_order,
+    copyright_owner_link,
+    original_title,
+    original_title_link
+  )
 
-uploadMedia3 <- function(url, df) {
-  
-  # Download all the files
-  #   media_files <- daply(df, c("sortOrder"), 
-  #     function(x) downloadMedia(x$url, x$mime))
-  
-  df <- unique(df)
-  
-  res <- ddply(df, c("sortOrder", "fileName"), 
-               function(x) downloadMedia(x$url, x$mime))
-  
-  media_files <- res$V1
-  
-  res$fileNameWExt <- res$V1
-  res$V1 <- NULL
-  
-  u <- join(df, res, by = c("fileName", "sortOrder"))
-  u$fileName <- NULL
-  u <- plyr::rename(u, replace = 
-    c("title" = "legend", "copyrightOwner" = "owner", "fileNameWExt" = "fileName"))  
-  u$access <- "public"
-  u$tags <- "origin:Naturforskaren"
-  
-  Encoding(u$owner) <- "latin1"
-  Encoding(u$legend) <- "latin1"
-  Encoding(u$tags) <- "latin1"
-  
-  u <- unique(u)
-    
-  res <- ddply(u, c("sortOrder", "fileName"), 
-    function(x) postMedia2(url, 
-      owner = x$owner, access = x$access, tags = x$tags, legend = x$legend,
-      language = x$language, licenseType = as.character(x$license),
-      fileName = as.character(x$fileName), export = FALSE, alt = x$legend))
-  
-  new_media_ids <- res$V1
-  names(new_media_ids) <- res$fileName
-  
-  # Delete temporary downloads
-  unlink(u$fileName)
-  
-  return (new_media_ids)
-}  
+birds <-   
+  mt %>% filter(nchar(copyright_owner_link) > 0) %>% 
+  arrange(taxon_uuid, url, sortOrder) #%>%
+#  select(url, taxon_uuid, copyright_owner_link, original_title, original_title_link)
 
-postMedia2 <- function(url, 
+#birds %>% distinct(copyright_owner_link)
+
+#browseURL(birds$url[1])  
+
+#  select uuid, original_title_link from media where original_title_link is not null group by uuid, original_title_link;
+
+birds <- 
+  birds %>% rename(
+    legend = original_title, 
+    owner = copyrightOwner, 
+    fileNameWExt = fileName
+  ) %>%
+  mutate(
+    access = "public",
+    tags = "origin:Naturforskaren"
+  ) %>% distinct()
+
+# downloadMedia(birds$url, birds$mime)
+
+
+media_download <- function(df, .pb = NULL) {
+  if (.pb$i < .pb$n) .pb$tick()$print()
+  fileName <- file.path(tempdir(), paste0(df$fileNameWExt, ".", df$mime))
+  res <- map2_int(df$url, fileName, function(x, y) 
+    download.file(url = x, method = "curl", destfile = y, quiet = TRUE))  
+  tibble(success = res == 0, path = fileName)
+}
+
+df <- birds #%>% slice(1:3)
+pb <- progress_estimated(nrow(df), 0)
+dls <- media_download(df, .pb = pb)
+
+#dls <- 
+#  tibble(fileName = dir("/tmp/RtmpbXvZlI", full.names = TRUE)) %>%
+#  mutate(fileNameWExt = gsub("/tmp/RtmpbXvZlI/(.*?)([.].*)", "\\1", fileName))
+
+dls2 <- 
+  dls %>% select(fileName = path) %>%
+  mutate(fileNameWExt = gsub("/tmp/RtmpnsflCr/(.*?)([.].*)", "\\1", fileName))
+
+
+df <- birds %>% inner_join(dls2)
+
+#browseURL(dls$path[3])
+
+dest_url <- "https://beta-media.dina-web.net/MediaServerResteasy/media/upload-file/base64"
+#dest_url2 <- "http://thinkpad.nrm.se:8080/MediaServerResteasy/media/upload-file/base64"
+
+library(httr)
+library(caTools)
+
+media_post <- function(destination, 
   owner = "Unknown owner",
   access = "Unknown access", 
   tags = "Unknown tags",
@@ -147,23 +181,13 @@ postMedia2 <- function(url,
   licenseType = "Unknown license type", 
   fileName = "Unknown filename", 
   export = FALSE, 
-  alt = "Unknown alt text") {
+  alt = "Unknown alt text",
+  copyright_owner_link = "Unknown owner link",
+  original_title = "Unknown title",
+  original_title_link = "Unknown title link"
+  ) {
 
-  require("httr")
-  require("caTools")
-
-  # TODO: shouldn't this use the url parameter?
-  destination <- paste0(
-    "http://ingimar-hp-elitebook-8470p.nrm.se",
-    ":8080", 
-    "/MediaServerResteasy",
-    #  "/media/post/json/utf8")
-    "/media/upload-file/base64")
-
-  destination <- "http://localhost:8080/MediaServerResteasy/media/upload-file/base64"
-
-  b64 <- system(command = paste("base64", fileName), intern = TRUE)
-  b64 <- paste(collapse = "", b64)
+  b64 <- base64encode(read_file_raw(fileName))
 
   body_json <- RJSONIO::toJSON(list(
     owner = owner,
@@ -175,142 +199,155 @@ postMedia2 <- function(url,
     fileName = fileName,
     fileDataBase64 = b64,
     export = export,
-    alt = alt)
-  )
-
+    alt = alt,
+    copyrightOwnerLink = copyright_owner_link,
+    originalTitle = original_title,
+    originalTitleLink = original_title_link
+  ))
+  
+  message("POST ", fileName, " ", original_title, " to ", destination)
+  
   res <- POST(destination, body = body_json, 
     add_headers("Content-Type" = "application/json"))
 
-  return (content(res, as = "text"))
+  if (status_code(res) != 201)
+    message("ERROR, POST result:", str(res))
+  
+  return (content(res, as = "text", encoding = "UTF-8"))
 }
 
-linkTaxonToMedias2 <- function(url, taxon_uuid, media_ids) {
+#  birds %>% filter(taxon_uuid == "004e6a45-c480-4420-9fcb-8225279094c0")
 
-  library(httr)
+res <- 
+  df %>%
+  rowwise() %>%
+  mutate(new_media_id = media_post(destination = dest_url,
+    owner = owner, access = access, tags = tags, legend = title,
+    language = language, licenseType = as.character(license),
+    fileName = as.character(fileName), export = FALSE, alt = title, 
+    copyright_owner_link = copyright_owner_link, 
+    original_title_link = original_title_link, 
+    original_title = legend))
+
+#saveRDS(res, "~/repos/dina-web/naturalist-docker/res.Rda")
+
+#rest <- res %>% slice(499:593)
+#rest <- res %>% slice(498)
+#final_res <- res %>% slice(1:498) %>% bind_rows(res2)
+#View(final_res)
+
+# clean dupes in mediadb looking at HASH
+
+pool <- dbPool(
+  drv = RMySQL::MySQL(),
+  dbname = "nf_media",
+  host = "127.0.0.1",
+  port = 13306,
+  username = "mediaserver",
+  password = rstudioapi::askForPassword() #   password = Sys.getenv("DINAPASS")
+)
+
+con <- poolCheckout(pool)
+dbGetQuery(con,'SET NAMES utf8')
+media_query <- "select * from MEDIA;"
+dupes <- as_tibble(collect(dbGetQuery(con, media_query)))
+poolClose(pool)
+
+temp <- 
+  dupes %>% group_by(HASH) %>% summarize(count = n()) %>% filter(count > 2) %>%
+  inner_join(dupes) %>% filter(count < 10) %>% arrange(desc(count)) %>%
+  filter(OWNER != "Bert Gustafsson") %>%
+  filter(!UUID %in% c("519aea58-fe73-4fef-9379-0978c2f668f4", "79851449-26af-4c40-a57f-9cd73f751fc5"))
+
+delete_det <- paste0("delete from DETERMINATION where MEDIA_UUID='", temp$UUID, "';")
+delete_med <- paste0("delete from MEDIA where UUID='", temp$UUID, "';")
+delete_lic <- paste0("delete from MEDIA_X_LIC where MEDIA_ID='", temp$UUID, "';")
+delete_img <- paste0("delete from IMAGE where UUID='", temp$UUID, "';")
+delete_tag <- paste0("delete from TAGS where MEDIA_UUID='", temp$UUID, "';")
+
+con <- poolCheckout(pool)
+dbGetQuery(con,'SET NAMES utf8')
+map(delete_det, function(x) dbGetQuery(con, x))
+map(delete_lic, function(x) dbGetQuery(con, x))
+map(delete_img, function(x) dbGetQuery(con, x))
+map(delete_tag, function(x) dbGetQuery(con, x))
+map(delete_med, function(x) dbGetQuery(con, x))
+poolClose(pool)
+
+media_taxon_link <- function(src_url, taxon_uuid, media_ids) {
+
+  destination <- paste0(
+    "https://beta-media.dina-web.net/MediaServerResteasy/", 
+    "media/postJSONWithLIsta"
+  )
   
-  destination <- paste0(url, "media/postJSONWithLIsta")
-  
-  body_json <- rjson::toJSON(list(
+  body_json <- RJSONIO::toJSON(x = list(
     typeOfSystem = "NF_TAXON",
     taxonUUID = taxon_uuid,
     nameOfSystem = "NF_SYSTEM",
     systemURL = src_url,
-    mediaList = media_ids
+    mediaList = I(media_ids)
   ))
-  
+  message(body_json)
   res <- POST(destination, body = body_json, 
-              add_headers("Content-Type" = "application/json"))
+    add_headers("Content-Type" = "application/json"))
     
   if (status_code(res) == 200) {
     res <- "OK (STATUS 200)"
   } else {
-    res <- NULL
+    message("ERROR: ", str(res))
+    res <- "ERROR"
   }
   
   return(res)
 }
 
-# Function that for a given a taxon identifier,
-# takes care of uploading the related media metadata
-# (media details and links) to the new media server over the web
-processTaxonUuidFromDB <- function(taxon_uuid) {
+#dupes %>% #filter(grepl("/tmp/", FILENAME)) %>% #slice(1) %>% t()
+#  mutate(uuid = gsub("/tmp/RtmpbXvZlI/(.*?)[.].*", "\\1", FILENAME)) %>%
+#  filter(HASH == "00cc9486cffcc1eee650b541a9ad66bb")
 
-  message("Processing taxon ", taxon_uuid)
+#dupes %>% filter(grepl("/tmp/", FILENAME)) %>% group_by(HASH) %>% summarize(count = n()) %>% filter(count > 1)
 
-  current_media <- getMediaUuidsForTaxonUuidFromDB(taxon_uuid)
-  
-  current_mime <- sapply(str_split(current_media$media_type, "/"), "[", 2)  
-  
-  current_url <- paste0(src_url, "/media/get-stream/", 
-    current_media$media_uuid)
-  
-  current_df <- data.frame(
-    copyrightOwner = current_media$copyright_owner,
-    license = current_media$license_abbrev,
-    title = current_media$legend,
-    fileName = current_media$media_uuid,
-    url = current_url, 
-    mime = current_mime,
-    language = current_media$language,
-    sortOrder = current_media$sort_order)
-
-  # Upload the medias to the media server
-
-  # FIXME! note that the upload function takes a parameter df 
-  # which requires a df with certain columns to be present
-  # not very pretty, should be changed
-   
-  media_ids <- paste(collapse = ", ", current_media$media_uuid)  
-  
-  message("Found nf media identifier(s) linked to this taxon: ", 
-       media_ids)  
-  
-  failed_upload <- paste0("Failed uploading media for taxon ",
-    taxon_uuid)  
-  mkUpload <- dplyr::failwith(NULL, uploadMedia3) 
-  uploaded_media_ids <- mkUpload(dest_url, current_df)  
-    
-  if (is.null(uploaded_media_ids)) 
-    return(failed_upload)
-
-  
-  # TODO! unicode characters
-  # TODO! sortorder works if only one language, but what if multiple language...
-  
-  u <- uploaded_media_ids
-  
-  # this is a named character vector with the sortOrder as
-  # the name attribute, we rearrange this to get the right order
-  
-  media_ids <- as.character(u)  # as.character(u[order(as.integer(names(u)))])
-  
-  message("Uploading related media for taxon ", taxon_uuid, 
-       "gave new media identifier(s) ", ": ", media_ids)
-  
-  # Link the taxon to the new media ids returned from the media server
-  
-  failed_link <- paste0("Failed linking taxon with identifier ", 
-    taxon_uuid, " to associated new media identifiers: ", media_ids)
-  mkLink <- dplyr::failwith(NULL, linkTaxonToMedias2)
-  res <- mkLink(dest_url, taxon_uuid, as.list(media_ids))
-    
-  if (is.null(res))
-    return(failed_link)
-  
-  message("Linking completed")
-  message("Linked taxon with identifier ", taxon_uuid, 
-       " to related media: ", res)
-  
-  return (res)
-}  
-
-# To debug with a restricted list of taxa, 
-# uncomment the lines below
-
-taxa_uuids <- c(
-# "fcffabdc-501e-489a-88af-fbd19d758dce",
-# "66118ad5-919b-4347-8981-e0944e6646f7"  
-# "3301bf9b-68c8-453f-b32d-8e99ba423290",
-# "2d5a6de0-e1c5-44cf-be16-044645d2a1c7"
-  "d8d88968-3282-4efc-9614-28e59a13ee69",
-  "6e18dcd9-a549-469d-8d9b-ee0f336b545a",
-  "e373547f-6f0d-42c4-9b40-7f7215e2c25f",
-  "56a83aa5-ac19-4d79-a1ad-32eb3aaf506e",
-  "263f8ef2-e085-408f-90b1-e6e4decfc4cb",
-  "1e90b156-00f9-4e73-a018-8ab67db956f5"
+mtl <- function(x) list(
+  taxon_uuid = x, 
+  media_ids = res %>% filter(taxon_uuid == x) %>% select(new_media_id) %>% .$new_media_id
 )
 
-#boho <- getMediaUuidsForTaxonUuidFromDB(taxa_uuids)
-#print(boho)
+linkz <- map(unique(res$taxon_uuid), mtl)
 
-message("Processing all media for all taxa")
-p <- plyr::failwith(NULL, processTaxonUuidFromDB, quiet = TRUE)
-res <- adply(taxa_uuids, 1, p, .progress = "text", .inform = TRUE)
+linkz_log <- map(linkz, function(x) media_taxon_link(
+  src_url = "https://naturforskaren.se",
+  taxon_uuid = x$taxon_uuid,
+  media_ids = x$media_ids
+  )
+)
 
-message("Done.")
+# are all with just one media_id actually not linked?
+# errorz <- linkz[which(linkz_log == "ERROR")]
+# 
+# errorz_linkz_log <- map(errorz[2:22], function(x) media_taxon_link(
+#   src_url = "https://naturforskaren.se",
+#   taxon_uuid = x$taxon_uuid,
+#   media_ids = x$media_ids
+#   )
+# )
 
-log <- data.frame(taxon = taxa_uuids, status = res$V1)
-message("Writing summary log to log.csv")
-write.csv(log, "log.csv", row.names = FALSE)
+# sanity checks... inspecting a few taxon uuids
+
+browse_uuids <- paste0( # gräsand! # björn!
+  "https://beta-naturforskaren.dina-web.net/nf-naturalist/species/", c(
+  "fcffabdc-501e-489a-88af-fbd19d758dce", # Sorgmantel
+  "66118ad5-919b-4347-8981-e0944e6646f7", # Sävvårtbitare
+  "3301bf9b-68c8-453f-b32d-8e99ba423290", # Mörkspräcklig hornmal
+  "2d5a6de0-e1c5-44cf-be16-044645d2a1c7", # Kohornsmal
+  "d8d88968-3282-4efc-9614-28e59a13ee69", # musslor
+  "6e18dcd9-a549-469d-8d9b-ee0f336b545a", # mård
+  "e373547f-6f0d-42c4-9b40-7f7215e2c25f", # hopprätvingar
+  "56a83aa5-ac19-4d79-a1ad-32eb3aaf506e", # björn
+  "263f8ef2-e085-408f-90b1-e6e4decfc4cb", # fjällämmel
+  "1e90b156-00f9-4e73-a018-8ab67db956f5" # hasselmus
+))
+
+map(browse_uuids, browseURL)
 
 if (!interactive()) q(status = 0)
